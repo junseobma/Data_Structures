a8215564a8ea2a546d5b94873be08275d517391d
majunseob <majunseob@gmail.com>
2025-02-07 22:39:33 +0900

    swap 생성

---
+++ C0101_Swap.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Swap two numbers
+	int a = 5;
+	int b = 10;
+
+	cout << "Before swapping: " << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	int temp = a;
+	a = b;
+	b = temp;
+
+	cout << "After swapping: " << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	return 0;
+}
